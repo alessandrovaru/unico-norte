9b86d640fd0d85dde888374bfcf4042dbd85f530
alessandrovaru <alessandrovaru@gmail.com>
2022-08-16 13:01:19 -0400

    creating a button that fetches to localhost

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Hello World</h1>
       </header>
+      <button onClick={() => {
+        fetch('http://localhost:3000/')
+          .then(res => res.json())
+          .then(data => console.log(data))
+      }
+      }>
+        Click Me
+      </button>
     </div>
   );
 }
