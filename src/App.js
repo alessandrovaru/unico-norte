f0b1af3211246621fcd38944fe6864e0ec652044
alessandrovaru <alessandrovaru@gmail.com>
2022-08-22 15:50:04 -0400

    Adding SASS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
+import './css/App.scss';
 //import useState from 'react';
 import { useState, useEffect } from 'react';
 
 function App() {
+const [logo, setLogo] = useState([]);
 const [titles, setTitles] = useState([]);
 
 useEffect(() => {
-  let cleanup = false;
+   let cleanup = false;
   fetch('http://localhost:3000/')
           .then(res => res.json())
-          .then(data => setTitles(data))
+          .then(data => setLogo(data))
   return () => {
     cleanup = true;
   };
@@ -20,6 +21,19 @@ useEffect(() => {
       <header>
         <h1>Lenguajio</h1>
       </header>
+      <button onClick={() => {
+        fetch('http://localhost:3000/list')
+          .then(res => res.json())
+          .then(data => setTitles(data))
+      }
+      }>
+        Click Me
+      </button>
+      {logo.map(logo => (
+        <div className='img_container'>
+          <img src={logo} alt={logo} />
+        </div>
+      ))}
       {titles.map(title => (
         <div className='img_container'>
           <img src={title} alt={title} />
