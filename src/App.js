bc28c2bcbca6d78a4f8ac975289978d174bf63b6
alessandrovaru <alessandrovaru@gmail.com>
2022-08-29 13:49:55 -0400

    Update structure

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
-import './css/App.scss';
-//import useState from 'react';
-import { useState, useEffect } from 'react';
+import './App.scss';
+
+import Header from './components/header/Header'
+import MainCarrousel from './components/main-carrousel/MainCarrousel';
 
 function App() {
-const [logo, setLogo] = useState([]);
-const [titles, setTitles] = useState([]);
+// const [logo, setLogo] = useState([]);
+// const [titles, setTitles] = useState([]);
+// const [error, setError] = useState([]);
 
-useEffect(() => {
-   let cleanup = false;
-  fetch('http://localhost:3000/')
-          .then(res => res.json())
-          .then(data => setLogo(data))
-  return () => {
-    cleanup = true;
-  };
-}, []);
+// useEffect(() => {
+
+//   fetch('http://localhost:3000/')
+//     .then(async response => {
+//         const isJson = response.headers.get('content-type')?.includes('application/json');
+//         const data = isJson && await response.json();
+//         // check for error response
+//         if (!response.ok) {
+//             // get error message from body or default to response status
+//             const err = (data && data.message) || response.status;
+//             setError(err)
+//             console.log(error)
+//             return Promise.reject(err);
+//         }
+//         setLogo(data)
+//     })
+//     .catch(error => {
+//         console.error('There was an error!', error);
+//     });
+//   return () => {
+//   };
+// }, [error]);
 
   return (
     <div className="App">
-      <header>
-        <h1>Lenguajio</h1>
-      </header>
-      <button onClick={() => {
-        fetch('http://localhost:3000/list')
-          .then(res => res.json())
-          .then(data => setTitles(data))
-      }
-      }>
-        Click Me
-      </button>
-      {logo.map(logo => (
-        <div className='img_container'>
-          <img src={logo} alt={logo} />
-        </div>
-      ))}
-      {titles.map(title => (
-        <div className='img_container'>
-          <img src={title} alt={title} />
-        </div>
-      ))}
+      <Header />
+      <MainCarrousel />
     </div>
   );
 }
---
+++ src/components/main-carrousel/MainCarrousel.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import './styles.scss'
+
+const MainCarrousel = () => {
+  return (
+    <div className='main-carrousel'>
+      <Container>
+        <div className="main-div">
+          <div>
+            Main Div
+          </div>
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default MainCarrousel
